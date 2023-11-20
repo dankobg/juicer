f82934ae75b70d41a129a4b8fc2ddaa3779f179f
Danko Petrovic <dankopetrovic.bgd@gmail.com>
2023-11-20 20:46:10 +0100

    dsad

--- engine/zzz_test.go
+++ engine/zzz_test.go
@@ -1,50 +1,16 @@
 package juicer
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestJuicer(t *testing.T) {
-	var wk, wq, wr, wb, wn, wp, bk, bq, br, bb, bn, bp bitboard
+	InitAllAttackMasksTables()
 
-	wk.setBit(E1)
-	wq.setBit(D1)
-	wr.setBit(A1)
-	wr.setBit(H1)
-	wb.setBit(C1)
-	wb.setBit(F1)
-	wn.setBit(B1)
-	wn.setBit(G1)
-	for _, x := range []Square{A2, B2, C2, D2, E2, F2, G2, H2} {
-		wp.setBit(x)
-	}
-	bk.setBit(E8)
-	bq.setBit(D8)
-	br.setBit(A8)
-	br.setBit(H8)
-	bb.setBit(C8)
-	bb.setBit(F8)
-	bn.setBit(B8)
-	bn.setBit(G8)
-	for _, x := range []Square{A7, B7, C7, D7, E7, F7, G7, H7} {
-		bp.setBit(x)
-	}
+	occ := bitboardEmpty
+	occ.setBit(D4)
+	occ.setBit(F5)
 
-	b := Board{
-		whiteKingOccupancy:    wk,
-		whiteQueensOccupancy:  wq,
-		whiteRooksOccupancy:   wr,
-		whiteBishopsOccupancy: wb,
-		whiteKnightsOccupancy: wn,
-		whitePawnsOccupancy:   wp,
-		blackKingOccupancy:    bk,
-		blackQueensOccupancy:  bq,
-		blackRooksOccupancy:   br,
-		blackBishopsOccupancy: bb,
-		blackKnightsOccupancy: bn,
-		blackPawnsOccupancy:   bp,
-	}
-
-	_ = b
-	// fmt.Println(b.Draw(nil))
+	fmt.Println(getQueenAttacks(E4, occ).draw(nil))
 }
--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"time"
-
-	juicer "github.com/dankobg/juicer/engine"
-)
-
 func main() {
-	now := time.Now()
 
-	for i := 0; i < 64; i++ {
-		m := juicer.FindMagicNum(i, true)
-		fmt.Printf("bishop: %v 0x%v\n", i, strconv.FormatUint(uint64(m), 16))
-	}
-
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-
-	for i := 0; i < 64; i++ {
-		m := juicer.FindMagicNum(i, false)
-		fmt.Printf("bishop: %v 0x%v\n", i, strconv.FormatUint(uint64(m), 16))
-	}
-
-	fmt.Println(time.Since(now))
 }
